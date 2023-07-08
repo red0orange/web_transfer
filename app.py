dad0799c5a800a572f8c90a5ba7e5b548bb8ec02
red0orange <huangdehao919@gmail.com>
2023-07-08 19:48:32 +0800

    version 0.5

--- app.py
+++ app.py
@@ -5,15 +5,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+# Save all messages to this list
+messages = []
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', messages=messages)
 
 @socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
+    # Save the message to the list
+    messages.append(message)
     send(message, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0')
 
